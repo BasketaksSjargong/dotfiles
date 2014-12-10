fbd07e171489338ad07ea551a0f6ce38491e9a28
Ivar Stangeby <istangeby@gmail.com>
2014-12-10 12:13:31 +0100

    Fixed the wallpapering, sets an image based on time. This needs to be run every 1-2 hours or so.

--- wallpapers/wallpaper_time.sh
+++ wallpapers/wallpaper_time.sh
@@ -4,14 +4,14 @@
 # Wallpapers Source: http://imgur.com/a/VZ9H2 
 
 TIME=$(date +"%H")
-FOLDER='~/ivar/dotfiles/wallpapers'
+FOLDER='/home/ivar/dotfiles/wallpapers'
 export DISPLAY=:0.0 # run "env | grep DISPLAY"
 
 case ${TIME} in
-    0[5-7])         feh --bg-fill ${FOLDER}/2.png ;;
-    0[8-9]|1[0-1])  feh --bg-fill ${FOLDER}/3.png ;;
-    1[2-6])         feh --bg-fill ${FOLDER}/4.png ;;
-    1[7-9]|20)      feh --bg-fill ${FOLDER}/5.png ;;
-    21)             feh --bg-fill ${FOLDER}/6.png ;;
-    2[2-3]|0[0-4])  feh --bg-fill ${FOLDER}/7.png ;;
+    0[5-7])         feh --bg-scale ${FOLDER}/2.png ;;
+    0[8-9]|1[0-1])  feh --bg-scale ${FOLDER}/3.png ;;
+    1[2-6])         feh --bg-scale ${FOLDER}/4.png ;;
+    1[7-9]|20)      feh --bg-scale ${FOLDER}/5.png ;;
+    21)             feh --bg-scale ${FOLDER}/6.png ;;
+    2[2-3]|0[0-4])  feh --bg-scale ${FOLDER}/7.png ;;
 esac
